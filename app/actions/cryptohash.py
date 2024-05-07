11667eeb2aed178ec71a2a528047b1f16711e5ce
Chris Ihure <ihure.chris@gmail.com>
2024-05-07 14:12:29 +0300

    made updates

--- app/actions/cryptohash.py
+++ app/actions/cryptohash.py
@@ -2,12 +2,14 @@ from utils.util import find_nodes
 from Crypto.Hash import SHA3_256
 import json
 
+
 def _compute_hash(msg):
     return SHA3_256.new(msg).hexdigest()
 
+
 def _hash_nodes(node, key, value):
-    if (key in list(node.keys())):
-        #print(f'Found {key} in {node}')
+    if key in list(node.keys()):
+        # print(f'Found {key} in {node}')
         if isinstance(node[key], list):
             for idx, data in enumerate(node[key]):
                 if data == value:
@@ -23,10 +25,11 @@ def _hash_nodes(node, key, value):
                 node_str = node[key]
             node[key] = _compute_hash(node_str.encode())
 
+
 def cryptohash_by_path(resource, el, params):
     ret = resource
-    path = el['path'] # "Patient.name"
-    path = path.split('.')[1:] # Remove root
+    path = el['path']  # "Patient.name"
+    path = path.split('.')[1:]  # Remove root
     if len(path) == 0:
         ret.clear()
         return
