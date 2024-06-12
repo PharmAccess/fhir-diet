a244409fc8d9f4bd3ef27f2697b76a3eeb5d332c
Chris Ihure <ihure.chris@gmail.com>
2024-06-12 09:11:32 +0300

    use original method and add output to file option

--- app/cli.py
+++ app/cli.py
@@ -1,30 +1,28 @@
 import typer
-from processor import process_data
-from utils.util import read_resource_from_file
+
 import config
-from rich import print
+from processor import process_data
+from utils.logger_wrapper import init_logger, get_logger
+from utils.util import read_resource_from_file, write_resource_to_file
 
 app = typer.Typer()
+log = get_logger()
 
 
 @app.command()
-def process_json(resource_filename: str, config_filename: str = typer.Argument("config.yaml")):
+def process(
+        resource_filename: str,
+        config_filename: str = typer.Argument("config.yaml"),
+        output_to_file: bool = False
+):
     resource = read_resource_from_file(resource_filename)
     settings = config.Settings(config_filename)
     ret = process_data(resource, settings)
-    print(f'CLI Process Result={ret}')
-
-
-@app.command()
-def process_ndjson(ndjson_filename: str, config_filename: str = typer.Argument("config.yaml")):
-    settings = config.Settings(config_filename)
-    with open(ndjson_filename, 'r') as f:
-        for line in f:
-            resource = eval(line)
-            ret = process_data(resource, settings)
-            print(f'CLI Process Result={ret}')
-            break
+    log.debug(f'CLI Process Result={ret}')
+    if output_to_file:
+        write_resource_to_file(resource_filename, ret)
 
 
 if __name__ == "__main__":
+    init_logger(5)
     app()
